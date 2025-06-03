c39d5285636e6bcb3a9437e9f19f9077c9b12f5f
evann gautron <gautron.evann@gmail.com>
2025-06-03 21:34:41 +0200

    Phase 2 : Interfaces

---
+++ gestionPersonnelSLAM/FrmAbsence.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace gestionPersonnelSLAM
+{
+    partial class FrmAbsence
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.grpGestionAbsence = new System.Windows.Forms.GroupBox();
+            this.grpAbsence = new System.Windows.Forms.GroupBox();
+            this.btnAffichePersonnel = new System.Windows.Forms.Button();
+            this.btnAjoutAbsence = new System.Windows.Forms.Button();
+            this.btnModifierAbsence = new System.Windows.Forms.Button();
+            this.btnSupprimmerAbsence = new System.Windows.Forms.Button();
+            this.dtpDebut = new System.Windows.Forms.DateTimePicker();
+            this.dtpFin = new System.Windows.Forms.DateTimePicker();
+            this.cboMotif = new System.Windows.Forms.ComboBox();
+            this.lblDebut = new System.Windows.Forms.Label();
+            this.lblFin = new System.Windows.Forms.Label();
+            this.lblMotif = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.grpGestionAbsence.SuspendLayout();
+            this.grpAbsence.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(764, 268);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // grpGestionAbsence
+            // 
+            this.grpGestionAbsence.Controls.Add(this.btnSupprimmerAbsence);
+            this.grpGestionAbsence.Controls.Add(this.btnModifierAbsence);
+            this.grpGestionAbsence.Controls.Add(this.btnAjoutAbsence);
+            this.grpGestionAbsence.Controls.Add(this.btnAffichePersonnel);
+            this.grpGestionAbsence.Controls.Add(this.dataGridView1);
+            this.grpGestionAbsence.Location = new System.Drawing.Point(12, 12);
+            this.grpGestionAbsence.Name = "grpGestionAbsence";
+            this.grpGestionAbsence.Size = new System.Drawing.Size(776, 372);
+            this.grpGestionAbsence.TabIndex = 1;
+            this.grpGestionAbsence.TabStop = false;
+            this.grpGestionAbsence.Text = "gestionDes Absences";
+            // 
+            // grpAbsence
+            // 
+            this.grpAbsence.Controls.Add(this.lblMotif);
+            this.grpAbsence.Controls.Add(this.lblFin);
+            this.grpAbsence.Controls.Add(this.lblDebut);
+            this.grpAbsence.Controls.Add(this.cboMotif);
+            this.grpAbsence.Controls.Add(this.dtpFin);
+            this.grpAbsence.Controls.Add(this.dtpDebut);
+            this.grpAbsence.Location = new System.Drawing.Point(12, 390);
+            this.grpAbsence.Name = "grpAbsence";
+            this.grpAbsence.Size = new System.Drawing.Size(776, 214);
+            this.grpAbsence.TabIndex = 2;
+            this.grpAbsence.TabStop = false;
+            this.grpAbsence.Text = "Gestion des absences";
+            // 
+            // btnAffichePersonnel
+            // 
+            this.btnAffichePersonnel.Location = new System.Drawing.Point(28, 331);
+            this.btnAffichePersonnel.Name = "btnAffichePersonnel";
+            this.btnAffichePersonnel.Size = new System.Drawing.Size(114, 23);
+            this.btnAffichePersonnel.TabIndex = 1;
+            this.btnAffichePersonnel.Text = "Afficher le personnel";
+            this.btnAffichePersonnel.UseVisualStyleBackColor = true;
+            // 
+            // btnAjoutAbsence
+            // 
+            this.btnAjoutAbsence.Location = new System.Drawing.Point(335, 331);
+            this.btnAjoutAbsence.Name = "btnAjoutAbsence";
+            this.btnAjoutAbsence.Size = new System.Drawing.Size(121, 23);
+            this.btnAjoutAbsence.TabIndex = 2;
+            this.btnAjoutAbsence.Text = "Ajouter une absence";
+            this.btnAjoutAbsence.UseVisualStyleBackColor = true;
+            // 
+            // btnModifierAbsence
+            // 
+            this.btnModifierAbsence.Location = new System.Drawing.Point(481, 331);
+            this.btnModifierAbsence.Name = "btnModifierAbsence";
+            this.btnModifierAbsence.Size = new System.Drawing.Size(118, 23);
+            this.btnModifierAbsence.TabIndex = 3;
+            this.btnModifierAbsence.Text = "Modifier une absence";
+            this.btnModifierAbsence.UseVisualStyleBackColor = true;
+            // 
+            // btnSupprimmerAbsence
+            // 
+            this.btnSupprimmerAbsence.Location = new System.Drawing.Point(621, 331);
+            this.btnSupprimmerAbsence.Name = "btnSupprimmerAbsence";
+            this.btnSupprimmerAbsence.Size = new System.Drawing.Size(137, 23);
+            this.btnSupprimmerAbsence.TabIndex = 4;
+            this.btnSupprimmerAbsence.Text = "Supprimmer une absence";
+            this.btnSupprimmerAbsence.UseVisualStyleBackColor = true;
+            // 
+            // dtpDebut
+            // 
+            this.dtpDebut.Location = new System.Drawing.Point(154, 60);
+            this.dtpDebut.Name = "dtpDebut";
+            this.dtpDebut.Size = new System.Drawing.Size(200, 20);
+            this.dtpDebut.TabIndex = 0;
+            // 
+            // dtpFin
+            // 
+            this.dtpFin.Location = new System.Drawing.Point(154, 125);
+            this.dtpFin.Name = "dtpFin";
+            this.dtpFin.Size = new System.Drawing.Size(200, 20);
+            this.dtpFin.TabIndex = 1;
+            // 
+            // cboMotif
+            // 
+            this.cboMotif.FormattingEnabled = true;
+            this.cboMotif.Location = new System.Drawing.Point(523, 86);
+            this.cboMotif.Name = "cboMotif";
+            this.cboMotif.Size = new System.Drawing.Size(121, 21);
+            this.cboMotif.TabIndex = 2;
+            // 
+            // lblDebut
+            // 
+            this.lblDebut.AutoSize = true;
+            this.lblDebut.Location = new System.Drawing.Point(61, 60);
+            this.lblDebut.Name = "lblDebut";
+            this.lblDebut.Size = new System.Drawing.Size(81, 13);
+            this.lblDebut.TabIndex = 3;
+            this.lblDebut.Text = "Date de debut :";
+            // 
+            // lblFin
+            // 
+            this.lblFin.AutoSize = true;
+            this.lblFin.Location = new System.Drawing.Point(77, 125);
+            this.lblFin.Name = "lblFin";
+            this.lblFin.Size = new System.Drawing.Size(65, 13);
+            this.lblFin.TabIndex = 4;
+            this.lblFin.Text = "Date de fin :";
+            // 
+            // lblMotif
+            // 
+            this.lblMotif.AutoSize = true;
+            this.lblMotif.Location = new System.Drawing.Point(481, 89);
+            this.lblMotif.Name = "lblMotif";
+            this.lblMotif.Size = new System.Drawing.Size(36, 13);
+            this.lblMotif.TabIndex = 5;
+            this.lblMotif.Text = "Motif :";
+            // 
+            // FrmAbsence
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 616);
+            this.Controls.Add(this.grpAbsence);
+            this.Controls.Add(this.grpGestionAbsence);
+            this.Name = "FrmAbsence";
+            this.Text = "FrmAbsence";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.grpGestionAbsence.ResumeLayout(false);
+            this.grpAbsence.ResumeLayout(false);
+            this.grpAbsence.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox grpGestionAbsence;
+        private System.Windows.Forms.GroupBox grpAbsence;
+        private System.Windows.Forms.Button btnSupprimmerAbsence;
+        private System.Windows.Forms.Button btnModifierAbsence;
+        private System.Windows.Forms.Button btnAjoutAbsence;
+        private System.Windows.Forms.Button btnAffichePersonnel;
+        private System.Windows.Forms.Label lblMotif;
+        private System.Windows.Forms.Label lblFin;
+        private System.Windows.Forms.Label lblDebut;
+        private System.Windows.Forms.ComboBox cboMotif;
+        private System.Windows.Forms.DateTimePicker dtpFin;
+        private System.Windows.Forms.DateTimePicker dtpDebut;
+    }
+}
--- gestionPersonnelSLAM/view/Form1.Designer.cs
+++ gestionPersonnelSLAM/view/Form1.Designer.cs
@@ -28,21 +28,223 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.grpGestion = new System.Windows.Forms.GroupBox();
+            this.dgvGestion = new System.Windows.Forms.DataGridView();
+            this.btnAfficheAbsence = new System.Windows.Forms.Button();
+            this.grpPersonnel = new System.Windows.Forms.GroupBox();
+            this.btnAjoutPersonnel = new System.Windows.Forms.Button();
+            this.btnModifierPersonnel = new System.Windows.Forms.Button();
+            this.btnSupprimmerPersonnel = new System.Windows.Forms.Button();
+            this.cboService = new System.Windows.Forms.ComboBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.txtPrenom = new System.Windows.Forms.TextBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.lnlNom = new System.Windows.Forms.Label();
+            this.lblTel = new System.Windows.Forms.Label();
+            this.lblPrenom = new System.Windows.Forms.Label();
+            this.lblMail = new System.Windows.Forms.Label();
+            this.lblService = new System.Windows.Forms.Label();
+            this.grpGestion.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGestion)).BeginInit();
+            this.grpPersonnel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // grpGestion
+            // 
+            this.grpGestion.Controls.Add(this.btnSupprimmerPersonnel);
+            this.grpGestion.Controls.Add(this.btnModifierPersonnel);
+            this.grpGestion.Controls.Add(this.btnAjoutPersonnel);
+            this.grpGestion.Controls.Add(this.btnAfficheAbsence);
+            this.grpGestion.Controls.Add(this.dgvGestion);
+            this.grpGestion.Location = new System.Drawing.Point(12, 12);
+            this.grpGestion.Name = "grpGestion";
+            this.grpGestion.Size = new System.Drawing.Size(776, 354);
+            this.grpGestion.TabIndex = 1;
+            this.grpGestion.TabStop = false;
+            this.grpGestion.Text = "Gestion globale";
+            // 
+            // dgvGestion
+            // 
+            this.dgvGestion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGestion.Location = new System.Drawing.Point(6, 29);
+            this.dgvGestion.Name = "dgvGestion";
+            this.dgvGestion.Size = new System.Drawing.Size(764, 261);
+            this.dgvGestion.TabIndex = 0;
+            // 
+            // btnAfficheAbsence
+            // 
+            this.btnAfficheAbsence.Location = new System.Drawing.Point(20, 311);
+            this.btnAfficheAbsence.Name = "btnAfficheAbsence";
+            this.btnAfficheAbsence.Size = new System.Drawing.Size(123, 23);
+            this.btnAfficheAbsence.TabIndex = 1;
+            this.btnAfficheAbsence.Text = "Afficher les absences";
+            this.btnAfficheAbsence.UseVisualStyleBackColor = true;
+            // 
+            // grpPersonnel
+            // 
+            this.grpPersonnel.Controls.Add(this.lblService);
+            this.grpPersonnel.Controls.Add(this.lblMail);
+            this.grpPersonnel.Controls.Add(this.lblPrenom);
+            this.grpPersonnel.Controls.Add(this.lblTel);
+            this.grpPersonnel.Controls.Add(this.lnlNom);
+            this.grpPersonnel.Controls.Add(this.txtMail);
+            this.grpPersonnel.Controls.Add(this.txtPrenom);
+            this.grpPersonnel.Controls.Add(this.txtTel);
+            this.grpPersonnel.Controls.Add(this.txtNom);
+            this.grpPersonnel.Controls.Add(this.cboService);
+            this.grpPersonnel.Location = new System.Drawing.Point(12, 372);
+            this.grpPersonnel.Name = "grpPersonnel";
+            this.grpPersonnel.Size = new System.Drawing.Size(776, 179);
+            this.grpPersonnel.TabIndex = 2;
+            this.grpPersonnel.TabStop = false;
+            this.grpPersonnel.Text = "Gestion du personnel";
+            // 
+            // btnAjoutPersonnel
+            // 
+            this.btnAjoutPersonnel.Location = new System.Drawing.Point(331, 311);
+            this.btnAjoutPersonnel.Name = "btnAjoutPersonnel";
+            this.btnAjoutPersonnel.Size = new System.Drawing.Size(112, 23);
+            this.btnAjoutPersonnel.TabIndex = 2;
+            this.btnAjoutPersonnel.Text = "Ajouter un personnel";
+            this.btnAjoutPersonnel.UseVisualStyleBackColor = true;
+            // 
+            // btnModifierPersonnel
+            // 
+            this.btnModifierPersonnel.Location = new System.Drawing.Point(469, 311);
+            this.btnModifierPersonnel.Name = "btnModifierPersonnel";
+            this.btnModifierPersonnel.Size = new System.Drawing.Size(133, 23);
+            this.btnModifierPersonnel.TabIndex = 3;
+            this.btnModifierPersonnel.Text = "Modifier un personnel";
+            this.btnModifierPersonnel.UseVisualStyleBackColor = true;
+            // 
+            // btnSupprimmerPersonnel
+            // 
+            this.btnSupprimmerPersonnel.Location = new System.Drawing.Point(626, 311);
+            this.btnSupprimmerPersonnel.Name = "btnSupprimmerPersonnel";
+            this.btnSupprimmerPersonnel.Size = new System.Drawing.Size(136, 23);
+            this.btnSupprimmerPersonnel.TabIndex = 4;
+            this.btnSupprimmerPersonnel.Text = "Supprimmer un personnel";
+            this.btnSupprimmerPersonnel.UseVisualStyleBackColor = true;
+            // 
+            // cboService
+            // 
+            this.cboService.FormattingEnabled = true;
+            this.cboService.Location = new System.Drawing.Point(606, 72);
+            this.cboService.Name = "cboService";
+            this.cboService.Size = new System.Drawing.Size(121, 21);
+            this.cboService.TabIndex = 0;
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(131, 35);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(100, 20);
+            this.txtNom.TabIndex = 1;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(131, 113);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(100, 20);
+            this.txtTel.TabIndex = 2;
+            // 
+            // txtPrenom
+            // 
+            this.txtPrenom.Location = new System.Drawing.Point(371, 35);
+            this.txtPrenom.Name = "txtPrenom";
+            this.txtPrenom.Size = new System.Drawing.Size(100, 20);
+            this.txtPrenom.TabIndex = 3;
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(371, 113);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(100, 20);
+            this.txtMail.TabIndex = 4;
+            // 
+            // lnlNom
+            // 
+            this.lnlNom.AutoSize = true;
+            this.lnlNom.Location = new System.Drawing.Point(75, 42);
+            this.lnlNom.Name = "lnlNom";
+            this.lnlNom.Size = new System.Drawing.Size(35, 13);
+            this.lnlNom.TabIndex = 5;
+            this.lnlNom.Text = "Nom :";
+            // 
+            // lblTel
+            // 
+            this.lblTel.AutoSize = true;
+            this.lblTel.Location = new System.Drawing.Point(82, 116);
+            this.lblTel.Name = "lblTel";
+            this.lblTel.Size = new System.Drawing.Size(28, 13);
+            this.lblTel.TabIndex = 6;
+            this.lblTel.Text = "Tel :";
+            // 
+            // lblPrenom
+            // 
+            this.lblPrenom.AutoSize = true;
+            this.lblPrenom.Location = new System.Drawing.Point(310, 41);
+            this.lblPrenom.Name = "lblPrenom";
+            this.lblPrenom.Size = new System.Drawing.Size(49, 13);
+            this.lblPrenom.TabIndex = 7;
+            this.lblPrenom.Text = "Prenom :";
+            // 
+            // lblMail
+            // 
+            this.lblMail.AutoSize = true;
+            this.lblMail.Location = new System.Drawing.Point(327, 120);
+            this.lblMail.Name = "lblMail";
+            this.lblMail.Size = new System.Drawing.Size(32, 13);
+            this.lblMail.TabIndex = 8;
+            this.lblMail.Text = "Mail :";
+            // 
+            // lblService
+            // 
+            this.lblService.AutoSize = true;
+            this.lblService.Location = new System.Drawing.Point(537, 75);
+            this.lblService.Name = "lblService";
+            this.lblService.Size = new System.Drawing.Size(49, 13);
+            this.lblService.TabIndex = 9;
+            this.lblService.Text = "Service :";
             // 
             // FrmGestion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 560);
+            this.Controls.Add(this.grpPersonnel);
+            this.Controls.Add(this.grpGestion);
             this.Name = "FrmGestion";
             this.Text = "Gestion du personnel";
             this.Load += new System.EventHandler(this.FrmGestion_Load);
+            this.grpGestion.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGestion)).EndInit();
+            this.grpPersonnel.ResumeLayout(false);
+            this.grpPersonnel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpGestion;
+        private System.Windows.Forms.DataGridView dgvGestion;
+        private System.Windows.Forms.Button btnAfficheAbsence;
+        private System.Windows.Forms.GroupBox grpPersonnel;
+        private System.Windows.Forms.Button btnSupprimmerPersonnel;
+        private System.Windows.Forms.Button btnModifierPersonnel;
+        private System.Windows.Forms.Button btnAjoutPersonnel;
+        private System.Windows.Forms.ComboBox cboService;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.TextBox txtPrenom;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.Label lblMail;
+        private System.Windows.Forms.Label lblPrenom;
+        private System.Windows.Forms.Label lblTel;
+        private System.Windows.Forms.Label lnlNom;
+        private System.Windows.Forms.Label lblService;
     }
 }
 
